92909cc0aebd9b5dc44ff80498fc405535261ede
akram-sa104 <akramsuryaalfarras@gmail.com>
2025-05-09 16:55:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/monexapp/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.userinterface.monexapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.monexapp.DashboardActivity
+import com.example.monexapp.R
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var usernameInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_activiy)
+
+        usernameInput = findViewById(R.id.etEmail)
+        passwordInput = findViewById(R.id.etPassword)
+        loginButton = findViewById(R.id.etlogin)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username == "admin" && password == "12345") {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Login gagal, akun tidak ditemukan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
